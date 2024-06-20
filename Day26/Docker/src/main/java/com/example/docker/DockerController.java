e2d4a6adac0e903ded6ec75af229c21a80ff3a2e
Aravinthan M <128985233+Aravinthanmurugan@users.noreply.github.com>
2024-06-20 10:38:14 +0530

    mm

---
+++ Day26/Docker/src/main/java/com/example/docker/DockerController.java
@@ -0,0 +1,17 @@
+package com.example.docker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello Method is called";
+	}
+	@GetMapping("/byee")
+	public String byee() {
+		return "byeeee Method is called";
+	}
+}
